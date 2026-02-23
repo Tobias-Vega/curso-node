76bed492a3d38524a6de760f0c2f80dc81317752
Tobias Vega <tobiasvega1210@gmail.com>
2026-02-23 13:34:07 -0300

    Promesas en cadena

--- 02-bases/src/app.js
+++ 02-bases/src/app.js
@@ -1,5 +1,6 @@
 const { getPokemonById } = require('./js-foundation/06-promises');
 
-getPokemonById(4, (pokemon) => {
-  console.log({ pokemon });
-});
+getPokemonById(4)
+  .then((name) => console.log(name))
+  .catch((error) => console.log('Por favor intente de nuevo'))
+  .finally(() => console.log('Finalmente'));
--- 02-bases/src/js-foundation/06-promises.js
+++ 02-bases/src/js-foundation/06-promises.js
@@ -1,15 +1,10 @@
-
-const getPokemonById = (id, callback) => {
-
+const getPokemonById = (id) => {
   const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
 
-  fetch(url).then((response => {
-    response.json().then((pokemon) => {
-      callback(pokemon.name);
-    });
-  }))
-}
-
+  return fetch(url)
+    .then((response) => response.json())
+    .then((pokemon) => (pokemon.name));
+};
 
 module.exports = {
   getPokemonById,
