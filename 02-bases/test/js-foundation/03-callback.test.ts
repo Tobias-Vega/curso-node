f8694607fbfb3c4ac8d414b2d7a498aef892946d
Tobias Vega <tobiasvega1210@gmail.com>
2026-02-25 01:06:46 -0300

    Pruebas en 03-callbacks

---
+++ 02-bases/test/js-foundation/03-callback.test.ts
@@ -0,0 +1,19 @@
+import { getUserById } from "../../src/js-foundation/03-callbacks";
+
+
+describe('js-foundation/03-callback.ts', () => {
+
+  test('getUserById shoul return an error if user does not exist', (done) => {
+
+    const id = 10;
+
+    getUserById(id, (err, user) => {
+      expect(err).toBe(`User not found with id ${id}`);
+      expect(user).toBeUndefined();
+
+      done();
+    });
+
+  });
+
+});
