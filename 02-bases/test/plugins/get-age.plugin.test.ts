9de7392c47b764919e1af2229387b1329173229e
Tobias Vega <tobiasvega1210@gmail.com>
2026-02-25 21:46:30 -0300

    SpyOn - Métodos de objetos

--- 02-bases/test/plugins/get-age.plugin.test.ts
+++ 02-bases/test/plugins/get-age.plugin.test.ts
@@ -23,4 +23,18 @@ describe('plugins/get-age.plugin.ts', () => {
     expect(age).toBe(calculatedAge);
 
   });
+
+  test('getAge should return 0 years', () => {
+
+    const spy = jest.spyOn(Date.prototype, 'getFullYear').mockReturnValue(1995);
+
+    const birthdate = '1995-10-21';
+    const age = getAge(birthdate);
+
+    console.log({ age });
+
+    expect(age).toBe(0);
+    expect(spy).toHaveBeenCalledWith();
+
+  })
 });
