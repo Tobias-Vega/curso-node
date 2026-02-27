9e151cc55411db6e8a7d6510fc47ed7533092cd0
Tobias Vega <tobiasvega1210@gmail.com>
2026-02-27 20:48:09 -0300

    CreateTable - UseCase

--- 04-multiplication/src/presentation/server-app.ts
+++ 04-multiplication/src/presentation/server-app.ts
@@ -1,3 +1,4 @@
+import { CreateTable } from "../domain/use-cases/create-table.use-case";
 
 
 interface RunOptions {
@@ -8,9 +9,12 @@ interface RunOptions {
 
 export class ServerApp {
 
-  static run(options: RunOptions) {
+  static run({ base, limit, showTable }: RunOptions) {
     console.log('Server running...');
-    console.log({options});
+
+    const table = new CreateTable().execute({ base, limit });
+
+    if (showTable) console.log(table);
   }
 
 }
