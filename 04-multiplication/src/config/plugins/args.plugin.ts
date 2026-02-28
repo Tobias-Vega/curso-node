7e4669b14c326d1c6796699bb6484e98a77f5086
Tobias Vega <tobiasvega1210@gmail.com>
2026-02-27 21:18:56 -0300

    Solucíón de la tarea - Reforzamiento de lo aprendido

--- 04-multiplication/src/config/plugins/args.plugin.ts
+++ 04-multiplication/src/config/plugins/args.plugin.ts
@@ -20,7 +20,19 @@ export const yarg = yargs(hideBin(process.argv))
     default: false,
     describe: 'Show multiplication table'
   })
-  .check(( argv, options ) => {
+  .option('n', {
+    alias: 'name',
+    type: 'string',
+    default: 'multiplication-table',
+    describe: 'File name'
+  })
+  .option('d', {
+    alias: 'destination',
+    type: 'string',
+    default: 'outputs',
+    describe: 'File destination'
+  })
+  .check((argv, options) => {
 
     if (argv.b < 1) throw 'Error: base must be greater than 0';
 
