5f21f3de626690b5849a28346adfe97d283aeef4
Tobias Vega <tobiasvega1210@gmail.com>
2026-02-28 00:02:51 -0300

    Pruebas - SaveFile UseCase

---
+++ 04-multiplication/src/domain/use-cases/save-file.use-case.test.ts
@@ -0,0 +1,30 @@
+import { SaveFile } from './save-file.use-case';
+import fs from 'fs';
+
+describe('SaveFileUseCase', () => {
+
+  afterEach(() => {
+    fs.rmSync('outputs', { recursive: true });
+
+  });
+
+  test('should save file with default values', () => {
+
+    const saveFile = new SaveFile();
+    const filePath = 'outputs/table.txt';
+    const options = {
+      fileContent: 'test content'
+    }
+
+    const result = saveFile.execute(options);
+
+    const fileExists = fs.existsSync(filePath);
+    const fileContent = fs.readFileSync(filePath, { encoding: 'utf-8' });
+
+    expect(result).toBeTruthy();
+    expect(fileExists).toBeTruthy();
+    expect(fileContent).toBe(options.fileContent);
+
+  });
+
+});
