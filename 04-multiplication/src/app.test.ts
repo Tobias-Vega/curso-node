eabb05d218e8deb2cdb770a9e4dac50568fd832d
Tobias Vega <tobiasvega1210@gmail.com>
2026-03-01 19:33:44 -0300

    Pruebas App.ts - Fin sección 7

--- 04-multiplication/src/app.test.ts
+++ 04-multiplication/src/app.test.ts
@@ -1,7 +1,27 @@
+import { ServerApp } from './presentation/server-app';
 
 
-describe('App', () => {
-  it('should be true', () => {
-    expect(true).toBeTruthy();
+describe('Test App.ts', () => {
+
+
+  test('should call Server.run with values', async () => {
+
+    const serverRunMock = jest.fn();
+    ServerApp.run = serverRunMock;
+    process.argv = ['node', 'app.ts', '-b', '10', '-l', '5', '-s', '-n', 'test-file', '-d', 'test-destination'];
+
+    await import('./app');
+
+    expect(serverRunMock).toHaveBeenCalledWith({
+      base: 10,
+      limit: 5,
+      showTable: true,
+      fileName: 'test-file',
+      fileDestination: 'test-destination'
+    });
+
+
   });
-})
+
+
+});
