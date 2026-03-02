b2df0b7f7bc6f56a50a1b7359b14cb1071296e79
Tobias Vega <tobiasvega1210@gmail.com>
2026-03-02 11:15:11 -0300

    Refactorización y nueva propiedad

--- 05-NOC/src/app.ts
+++ 05-NOC/src/app.ts
@@ -7,7 +7,7 @@ import { Server } from "./presentation/server";
 
 function main() {
 
-  // Server.start();
+  Server.start();
   // console.log(envs);
 
 }
--- 05-NOC/src/presentation/server.ts
+++ 05-NOC/src/presentation/server.ts
@@ -14,7 +14,7 @@ export class Server {
     CronService.createJob(
       '*/5 * * * * *',
       () => {
-        const url = 'http://localhost:3000';
+        const url = 'https:google.com';
         new CheckService(
           fileSystemLogRepository,
           () => console.log(`${url} is ok`),
