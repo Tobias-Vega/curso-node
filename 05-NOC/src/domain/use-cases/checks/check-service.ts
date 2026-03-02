1045ce8e60f6b29d6ce212d7352a211a4ed1bfc3
Tobias Vega <tobiasvega1210@gmail.com>
2026-03-01 21:51:31 -0300

    Inyección de dependencias - Fin sección 8

--- 05-NOC/src/domain/use-cases/checks/check-service.ts
+++ 05-NOC/src/domain/use-cases/checks/check-service.ts
@@ -2,7 +2,16 @@ interface CheckServiceUseCase {
   execute(url: string): Promise<boolean>
 }
 
+type SuccessCallback = () => void;
+type ErrorCallback = (error: string) => void;
+
+
 export class CheckService implements CheckServiceUseCase {
+
+  constructor(
+    private readonly successCallback: SuccessCallback,
+    private readonly errorCallback: ErrorCallback,
+  ) {}
 
   async execute(url: string): Promise<boolean> {
 
@@ -14,13 +23,14 @@ export class CheckService implements CheckServiceUseCase {
         throw new Error(`Error on check service ${url}`);
       }
 
-      console.log(`${url} is ok`);
+      this.successCallback();
       
       return true;
     } catch (error) {
 
       console.log(`${error}`);
       
+      this.errorCallback(`${error}`);
 
       return false;
     }
