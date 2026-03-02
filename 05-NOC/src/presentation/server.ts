4d2aadb9d77a9259da556e72513c07416d2cdc5e
Tobias Vega <tobiasvega1210@gmail.com>
2026-03-02 09:40:11 -0300

    Implementación probada

--- 05-NOC/src/presentation/server.ts
+++ 05-NOC/src/presentation/server.ts
@@ -1,5 +1,9 @@
 import { CheckService } from "../domain/use-cases/checks/check-service";
+import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impletation";
 import { CronService } from "./cron/cron-service";
+import { FileSystemDatasource } from '../infrastructure/datasources/file-system.datasource';
+
+const fileSystemLogRepository = new LogRepositoryImpl(new FileSystemDatasource()); 
 
 export class Server {
 
@@ -10,8 +14,9 @@ export class Server {
     CronService.createJob(
       '*/5 * * * * *',
       () => {
-        const url = 'https://google.com';
+        const url = 'http://localhost:3000';
         new CheckService(
+          fileSystemLogRepository,
           () => console.log(`${url} is ok`),
           (error) => console.log(error),
         ).execute(url);
