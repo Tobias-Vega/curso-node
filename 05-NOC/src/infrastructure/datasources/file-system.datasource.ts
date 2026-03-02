9fa5829d23502fc36d166debafe5f9f1b5a28e67
Tobias Vega <tobiasvega1210@gmail.com>
2026-03-01 23:27:14 -0300

    FyleSistem - Datasource

---
+++ 05-NOC/src/infrastructure/datasources/file-system.datasource.ts
@@ -0,0 +1,40 @@
+import fs from 'fs';
+
+import { LogDatasource } from "../../domain/datasources/log.datasource";
+import { LogSeverityLevel, LogEntity } from "../../domain/entities/log.entity";
+
+
+export class FileSystemDatasource implements LogDatasource {
+
+  private readonly logPath = 'logs/';
+  private readonly allLogsPath = 'logs/logs-low.log';
+  private readonly mediumLogsPath = 'logs/logs-medium.log';
+  private readonly highLogsPath = 'logs/logs-high.log';
+
+  constructor() {
+    this.createLogsFiles();
+  }
+
+  private createLogsFiles = () => {
+    if (!fs.existsSync(this.logPath)) {
+      fs.mkdirSync(this.logPath);
+    }
+
+    [
+      this.allLogsPath,
+      this.mediumLogsPath,
+      this.highLogsPath,
+    ].forEach(path => {
+      if (fs.existsSync(path)) return;
+      fs.writeFileSync(path, '')
+    })
+  }
+
+  saveLogs(log: LogEntity): Promise<void> {
+    throw new Error('Method not implemented');
+  }
+
+  getLogs(serverityLevel: LogSeverityLevel): Promise<LogEntity[]> {
+    throw new Error('Method not implemented');
+  }
+}
