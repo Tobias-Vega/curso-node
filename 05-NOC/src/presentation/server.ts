86b0a88655c1194a582248c3897d6b740b3bc85d
Tobias Vega <tobiasvega1210@gmail.com>
2026-03-01 21:01:49 -0300

    CRON tasks

--- 05-NOC/src/presentation/server.ts
+++ 05-NOC/src/presentation/server.ts
@@ -1,10 +1,20 @@
-
+import { CronJob } from "cron";
 
 export class Server {
 
   public static start() {
 
     console.log('Server started...');
+
+    var job = new CronJob(
+      '*/2 * * * * *',
+      () => {
+        const date = new Date();
+        console.log('2 second', date);
+      }
+    );
+
+    job.start();
 
   }
 
