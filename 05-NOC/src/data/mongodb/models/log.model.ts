dff5eb7b120e71a1215ac8a21feaf4bdecf0b14f
Tobias Vega <tobiasvega1210@gmail.com>
2026-03-02 22:57:32 -0300

    Schema & Models Mongo

---
+++ 05-NOC/src/data/mongodb/models/log.model.ts
@@ -0,0 +1,24 @@
+import { model, Schema } from 'mongoose';
+
+const logSchema = new Schema({
+
+  message: {
+    type: String,
+    require: true,
+  },
+  origin: {
+    type: String,
+  },
+  level: {
+    type: String,
+    enum: ['low', 'medium', 'high'],
+    default: 'low',
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  }
+
+});
+
+export const logModel = model('Log', logSchema);
