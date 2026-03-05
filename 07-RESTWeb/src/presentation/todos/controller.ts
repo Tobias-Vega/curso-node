42178d8204ff4f0d9d45c1d6116f0862ec96433e
Tobias Vega <tobiasvega1210@gmail.com>
2026-03-05 01:45:55 -0300

    CRUD - Crear el TODO en el arreglo

--- 07-RESTWeb/src/presentation/todos/controller.ts
+++ 07-RESTWeb/src/presentation/todos/controller.ts
@@ -30,9 +30,18 @@ export class TodosController {
 
   public createTodo = (req: Request, res: Response) => {
 
-    const body = req.body;
+    const { text } = req.body;
 
-    
+    if (!text) return res.status(400).json({ error: 'Text property is required' });
 
+    const newTodo = {
+      id: todos.length + 1,
+      text: text,
+      createdAt: null,
+    };
+
+    todos.push(newTodo);
+
+    res.status(201).json(newTodo);
   }
 }
