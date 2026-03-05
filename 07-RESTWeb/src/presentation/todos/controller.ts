6df5686f236b5da7e280186f6a63e4d249633443
Tobias Vega <tobiasvega1210@gmail.com>
2026-03-05 02:10:05 -0300

    CRUD - Delete - Fin Sección 14

--- 07-RESTWeb/src/presentation/todos/controller.ts
+++ 07-RESTWeb/src/presentation/todos/controller.ts
@@ -63,4 +63,16 @@ export class TodosController {
     res.status(200).json(todo);
 
   }
+
+  public deleteTodo = (req: Request, res: Response) => {
+    const id = +req.params.id!;
+
+    if (!id || isNaN(id)) return res.status(400).json({ error: 'ID argument is not a number' });
+
+    const todo = todos.find(todo => todo.id === id);
+    if (!todo) return res.status(404).json({ error: `Todo with id ${id} not found` });
+
+    todos.splice(todos.indexOf(todo), 1);
+    res.status(204).send();
+  }
 }
