7e345889fc1ae5f3ec0f2bd6e149d7a767b15523
Tobias Vega <tobiasvega1210@gmail.com>
2026-03-06 21:42:38 -0300

    Prueba - GET - api/todos

--- 07-RESTWeb/test/presentation/routes.test.ts
+++ 07-RESTWeb/test/presentation/routes.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import { testServer } from '../test-server';
+import { prisma } from '../../src/lib/prisma';
 
 describe('Todo route testing', () => {
 
@@ -11,10 +12,25 @@ describe('Todo route testing', () => {
     testServer.close();
   });
 
+  const todo1 = { text: 'Hola Mundo 1' };
+  const todo2 = { text: 'Hola Mundo 2' };
+
   test('should return TODOs api/todos', async () => {
 
-    await request(testServer.app)
+    await prisma.todo.deleteMany();
+    await prisma.todo.createMany({
+      data: [todo1, todo2]
+    });
+
+    const { body } = await request(testServer.app)
       .get('/api/todos')
       .expect(200);
+
+    expect(body).toBeInstanceOf(Array);
+    expect(body.length).toBe(2);
+    expect(body[0].text).toBe(todo1.text);
+    expect(body[1].text).toBe(todo2.text);
+    expect(body[0].completedAt).toBeNull();
+    expect(body[1].completedAt).toBeNull();
   });
 });
