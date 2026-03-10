24629a04ede844260393c24673ad1b19c4125e50
Tobias Vega <tobiasvega1210@gmail.com>
2026-03-10 12:25:47 -0300

    Crear Category en base de datos

--- 08-user-store/src/data/mongodb/models/category.model.ts
+++ 08-user-store/src/data/mongodb/models/category.model.ts
@@ -5,7 +5,8 @@ const categorySchema = new Schema({
 
   name: {
     type: String,
-    required: [true, 'Name is required']
+    required: [true, 'Name is required'],
+    unique: true
   },
 
   available: {
