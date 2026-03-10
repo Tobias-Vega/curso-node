1430212414b41ba072949a025a3fe9268dbb1871
Tobias Vega <tobiasvega1210@gmail.com>
2026-03-10 11:29:39 -0300

    Category - Rutas y Controlador

---
+++ 08-user-store/src/presentation/category/routes.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import { CategoryController } from "./controller";
+
+export class CategoryRoutes {
+
+  static get routes(): Router {
+
+    const router = Router();
+
+    const categoryController = new CategoryController();
+
+    router.get('/', categoryController.getCategories);
+    router.post('/', categoryController.createCategory);
+
+    return router;
+  }
+}
