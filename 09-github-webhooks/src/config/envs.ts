8a61d02cc878d5e2a0096dd680ee171db01d0b9d
Tobias Vega <tobiasvega1210@gmail.com>
2026-03-13 16:15:30 -0300

    Generar secret token

--- 09-github-webhooks/src/config/envs.ts
+++ 09-github-webhooks/src/config/envs.ts
@@ -3,6 +3,7 @@ import env from 'env-var';
 
 export const envs = {
 
-  PORT: env.get('PORT').required().asPortNumber()
+  PORT: env.get('PORT').required().asPortNumber(),
+  SECRET_TOKEN: env.get('SECRET_TOKEN').required().asString(),
 
 }
