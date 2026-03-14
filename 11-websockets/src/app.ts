d0aff035a72035b4270612f663e163fb28ed2fec
Tobias Vega <tobiasvega1210@gmail.com>
2026-03-14 18:51:17 -0300

    Client Broadcast - A todos menos el emisor

--- 11-websockets/src/app.ts
+++ 11-websockets/src/app.ts
@@ -4,7 +4,7 @@ import { WebSocketServer, WebSocket } from 'ws';
 const wss = new WebSocketServer({ port: 3000 });
 
 wss.on('connection', function connection(ws) {
-  
+
   console.log('Client connected');
   ws.on('error', console.error);
 
@@ -16,8 +16,17 @@ wss.on('connection', function connection(ws) {
     });
 
     // ws.send(JSON.stringify(payload));
+
+    //* Todos - incluyente
+    // wss.clients.forEach(function each(client) {
+    //   if (client.readyState === WebSocket.OPEN) {
+    //     client.send(payload, { binary: false });
+    //   }
+    // });
+
+    // * Todos excluyente
     wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(payload, { binary: false });
       }
     });
