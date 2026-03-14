d5afe77174e7f7659604fe4f0688d2719ae962d6
Tobias Vega <tobiasvega1210@gmail.com>
2026-03-14 18:47:45 -0300

    Server Broadcast

--- 11-websockets/src/app.ts
+++ 11-websockets/src/app.ts
@@ -1,5 +1,5 @@
 
-import { WebSocketServer } from 'ws';
+import { WebSocketServer, WebSocket } from 'ws';
 
 const wss = new WebSocketServer({ port: 3000 });
 
@@ -9,15 +9,18 @@ wss.on('connection', function connection(ws) {
   ws.on('error', console.error);
 
   ws.on('message', function message(data) {
-    console.log('Desde el cliente:', data);
-    // Todo: enviar la data al cliente de regreso
 
-    const payload = {
+    const payload = JSON.stringify({
       type: 'custom-message',
       payload: data.toString(),
-    }
+    });
 
-    ws.send(JSON.stringify(payload));
+    // ws.send(JSON.stringify(payload));
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(payload, { binary: false });
+      }
+    });
 
   });
 
