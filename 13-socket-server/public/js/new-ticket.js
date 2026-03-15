9355e862057cb347abeebe2405dad6600f9b0ea7
Tobias Vega <tobiasvega1210@gmail.com>
2026-03-15 20:02:38 -0300

    Pantalla - Nuevo ticket

--- 13-socket-server/public/js/new-ticket.js
+++ 13-socket-server/public/js/new-ticket.js
@@ -1,3 +1,33 @@
 
+const currentTicketLabel = document.querySelector('span');
+const createTicketBtn = document.querySelector('button');
 
-console.log('Nuevo Ticket HTML');
+async function getLastTicket() {
+
+  const response = await fetch('http://localhost:3000/api/ticket/last');
+
+  const data = await response.json();
+
+  return data;
+}
+
+async function renderTicketNumber() {
+  const lastTicket = await getLastTicket();
+  currentTicketLabel.innerText = lastTicket;
+}
+
+async function createTicket() {
+
+  const newTicket = await fetch('http://localhost:3000/api/ticket', {
+    method: 'POST',
+  }).then(resp => resp.json());
+
+  currentTicketLabel.innerText = newTicket.number;
+  
+}
+
+
+createTicketBtn.addEventListener('click', createTicket)
+
+
+renderTicketNumber();
