e1a9477a101b1e1433f2f475b92c0d7affe36ac8
Tobias Vega <tobiasvega1210@gmail.com>
2026-03-15 20:36:27 -0300

    Detalles de la pantalla de escritorio

--- 13-socket-server/public/js/desk.js
+++ 13-socket-server/public/js/desk.js
@@ -1,13 +1,42 @@
 // Referencias HTML
 
 const lblPending = document.querySelector('#lbl-pending');
+const deskHeader = document.querySelector('h1');
+const noMoreAlert = document.querySelector('.alert');
+
+const searchParams = new URLSearchParams(window.location.search);
+
+
+if (!searchParams.has('escritorio')) {
+  window.location = 'index.html';
+
+  throw new Error('Escritorio es requerido');
+
+}
+
+const deskNumber = searchParams.get('escritorio');
+
+deskHeader.innerText = deskNumber;
+
+function checkTicketCount(currentCount = 0) {
+
+  if (currentCount === 0) {
+    noMoreAlert.classList.remove('d-none');
+
+  } else {
+    noMoreAlert.classList.add('d-none');
+  }
+
+  lblPending.innerHTML = currentCount;
+
+}
 
 async function loadInitialCount() {
-  const pending = await fetch('/api/ticket/pending').then((resp) =>
+  const pendingTickets = await fetch('/api/ticket/pending').then((resp) =>
     resp.json(),
   );
 
-  lblPending.innerHTML = pending.length || 0;
+  checkTicketCount(pendingTickets.length);
 }
 
 loadInitialCount();
@@ -21,7 +50,7 @@ function connectToWebSockets() {
 
     if (type !== 'on-ticket-count-changed');
 
-    lblPending.innerHTML = payload;
+    checkTicketCount(payload);
   };
 
   socket.onclose = (event) => {
